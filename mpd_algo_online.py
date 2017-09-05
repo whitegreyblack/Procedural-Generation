c65c8d553308a6c35b024b1d6f8315c1c807b7b8
Sam Whang <sangwoowhang@gmail.com>
2017-09-05 10:33:10 -0500

    Added some formatting

--- mpd_algo_online.py
+++ mpd_algo_online.py
@@ -1,14 +1,10 @@
-import tdl
 import math
 import copy
 import graph
 import color
 import graph
 import random
-import logging
 import movement
-
-logging.basicConfig(level=logging.DEBUG, filename='points.log', format='%(message)s')
 
 class MPD:
     def __init__(self, width, height, noise, delta):
@@ -30,31 +26,21 @@ class MPD:
         if x < 1 or y < 1: 
             return
         
-        # diamond
-        #self.world[x][y]
-
         self.diamondsquare((0,0), ((x9+x1)//2,(y9+y1)//2), delta)
 
 ''' initialize '''
 WIDTH, HEIGHT = 128, 128
-tdl.setFont('fonts/4x6.png')
-console = tdl.init(WIDTH, HEIGHT, 'heightmap')
 plane = MPD(WIDTH, HEIGHT, .8, 50)
 
 
 def level(x):
     return (250/x)*(x-1)
 
-while True:
-    console.clear()
-    mts = set()
-    for i in range(WIDTH):
-        for j in range(HEIGHT):
-            val = plane.world[i][j]
-            pass
-    tdl.flush()
-    for event in tdl.event.get():
-        if (event.type == 'KEYDOWN') and (event.keychar.lower() == 'q'):
-            raise SystemExit('The window has been closed.')
-        if event.type == 'QUIT':
-            raise SystemExit('The window has been closed.')
+mts = set()
+lines = []
+for i in range(WIDTH):
+    line = ""
+    for j in range(HEIGHT):
+        line += "." if line else " "
+    lines.append(line)
+print("\n".join(lines))
