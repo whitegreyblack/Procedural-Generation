555096b1cf5eb86f7d0ea1e0feacdb8559cd0b56
Sam Whang <sangwoowhang@gmail.com>
2017-12-18 14:52:01 -0600

    Cleaned up 1D mid point displacement script and replaced TDL code with BLT code instead

--- mpd_one_dim.py
+++ mpd_one_dim.py
@@ -1,73 +1,76 @@
-import tdl
 import math
 import copy
 import graph
 import color
 import graph
-import random
-import logging
 import movement
-
-logging.basicConfig(level=logging.DEBUG, filename='points.log', format='%(message)s')
+from random import randint, random
+from bearlibterminal import terminal
 
 class MPD:
     def __init__(self, width, height, noise):
         self.noise = noise
-        self.world = [random.random()*2-1 for i in range(width)]
+        self.world = [random() * 2-1 for i in range(width)]
         self.maxa = 0.0
         self.mini = 0.0
 
     def subdivide(self, x1, x2, delta):
-        if x1+1 == x2:
+        if x1 + 1 == x2:
             return
-        x3 = (x1+x2)/2
-        dy = (random.random()*2-1)*delta
+
+        x3 = (x1 + x2) // 2
+        dy = (random() * 2 - 1) * delta
 
         # midpoint
-        self.world[x3] = (self.world[x1]+self.world[x2])/2+dy
+        self.world[x3] = (self.world[x1] + self.world[x2]) / 2 + dy
+
         if self.world[x3] < self.mini:
             self.mini = self.world[x3]
+
         if self.world[x3] > self.maxa:
             self.maxa = self.world[x3]
-        self.subdivide(x1, x3, delta*self.noise)
-        self.subdivide(x3, x2, delta*self.noise)
+
+        self.subdivide(x1, x3, delta * self.noise)
+        self.subdivide(x3, x2, delta * self.noise)
 
     def serialdivide(self, x, end, delta):
         if x+2 == end:
             return
-        dy = (random.random()*2-1)*delta
-        self.world[x+1] = (self.world[x]+self.world[x+2])/2+dy
+
+        dy = (random() * 2-1) * delta
+        self.world[x + 1] = (self.world[x] + self.world[x + 2]) / 2 + dy
+
         if self.world[x+1] < self.mini:
             self.mini = self.world[x+1]
+
         if self.world[x+1] > self.maxa:
             self.maxa = self.world[x+1]
-        self.subdivide(x+1, end, delta*self.noise)
-        
+
+        self.subdivide(x + 1, end, delta * self.noise)
+
     def smooth(self, x1=None, x2=None):
         for x in range(x1 if x1 else 0, x2 if x2 else len(self.world)):
-            self.world[x] = (self.world[(x-1)%(len(self.world))]+self.world[x]+self.world[(x+1)%(len(self.world))])/3.0
-
-pvar = "{}: {}"
+            self.world[x] = (self.world[(x - 1) % len(self.world)] + self.world[x] + self.world[(x + 1) % len(self.world)]) / 3.0
 
 ''' initialize '''
-WIDTH, HEIGHT = 300, 128
-tdl.setFont('fonts/4x6.png')
-console = tdl.init(WIDTH, HEIGHT, 'heightmap')
+WIDTH, HEIGHT = 80, 50
+
+terminal.open()
+terminal.set(f'window: size={80}x{25}, cellsize=8x16')
 
 line = MPD(WIDTH, HEIGHT, .7)
-line.subdivide(0, WIDTH-1, 50)
-#line.serialdivide(0, WIDTH-1, 50)
-total = line.maxa-line.mini
+#line.subdivide(0, WIDTH-1, 50)
+
+line.serialdivide(0, WIDTH-1, 50)
+total = line.maxa - line.mini
+
 while True:
-    console.clear()
+    terminal.clear()
     for i in range(WIDTH):
-        console.draw_char(i, int(round((line.world[i]-line.mini)/(total)*HEIGHT)), 'o')
-        # console.draw_char(i, int(round(line.world[i] * HEIGHT/total + HEIGHT/total)), 'o')
-    tdl.flush()
-    for event in tdl.event.get():
-        if (event.type == 'KEYDOWN') and (event.keychar.lower() == 'q'):
-            raise SystemExit('The window has been closed.')
-        if (event.type == 'KEYDOWN') and (event.keychar.lower() == 's'):
-            line.smooth()
-        if event.type == 'QUIT':
-            raise SystemExit('The window has been closed.')
+        terminal.puts(i, int(round((line.world[i] - line.mini)) / (total) * HEIGHT), 'o')
+    terminal.refresh()
+    key = terminal.read()
+    if key in (terminal.TK_Q, terminal.TK_ESCAPE, terminal.TK_CLOSE):
+        break
+    elif key == terminal.TK_S:
+        line.smooth()
