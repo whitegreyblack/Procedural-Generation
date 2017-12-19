2855e9ab807873b127458846fcc2f363d6ca3765
Sam Whang <sangwoowhang@gmail.com>
2017-12-19 15:07:08 -0600

    Merged multiheight and drunkards and created a parent class that the maps inherit from

--- drunkards.py
+++ drunkards.py
@@ -1,16 +1,20 @@
-from PIL import Image, ImageDraw
-from bearlibterminal import terminal
-from mpd_one_dim import key_handle_exit, setup
 import math
 import color
-from random import choice, randint, random
-"""
-52666 40 20
-21096 100 80
-56330 100 80
-"""
+import random
+import randomfill
+from mpd_one_dim import setup, key_handle_exit
+from bearlibterminal import terminal
+from PIL import Image, ImageDraw
 
-def steps_4_way():
+''' map functions '''
+def steps_4_diagonal():
+    steps = set()
+    for i in range(-1, 2, 2):
+        for j in range(-1, 2, 2):
+            steps.add((i, j))
+    return steps
+
+def steps_4_lateral():
     steps = set()
     for i in range(-1, 2, 2):
         steps.add((0, i))
@@ -32,48 +36,179 @@ def steps_9_way():
             steps.add((i, j))
     return steps
 
-''' map functions '''
+def term_loop(m):
+    output_flag = False
+    while True:
+        # console.clear()
+        terminal.clear()
+        for x, y, ch, col in list(m.output_terminal(output_flag)):
+            terminal.puts(x, y, '[c={}]{}[/c]'.format(col, ch))
+        terminal.refresh()
+
+        key = terminal.read()
+        if key_handle_exit(key):
+            break
+        elif key == terminal.TK_S:
+            m.smooth()
+            m.normalize()
+            m.evaluate()
+        elif key == terminal.TK_F:
+            output_flag = not output_flag
+
 class Map:
-    def __init__(self, width, height, limit, seed=None):
-        self.x, self.y = width, height
-        self.limit = int(height * width * limit)
-        self.world = [[0 for _ in range(width)] for _ in range(height)]
-        self.seed = randint(0, 99999) if not seed else seed
-        self.build()
+    def __init__(self, width, height, seed=None):
+        self.width, self.height = width, height
+
+        self.world = self.create_base()
+        self.world_normal = self.create_base()
+        self.world_colored = self.create_base()
+
+        self.seed = seed if seed else random.randint(0, 99999)
+        random.seed(self.seed)
+
+    def  __iadd__(self, other):
+        if (self.width, self.height) != (other.width, other.height):
+            raise ValueError('World Input has different dimensions')
+        for y in range(self.height):
+            for x in range(self.width):
+                self.world[y][x] += other.world[y][x]
+        return self
+
+    def __imul__(self, other):
+        print(self.world[self.height//2][self.width//2], self.world[self.height//2][self.width//2])
+        if (self.width, self.height) != (other.width, other.height):
+            raise ValueError('World Input has different dimensions')
+        for y in range(self.height):
+            for x in range(self.width):
+                self.world[y][x] *= other.world[y][x]
+        return self       
+
+    def create_base(self):
+        return [[0 for _ in range(self.width)] for _ in range(self.height)]
 
     def check_bounds(self, x, y):
-        return 0 <= x < self.x and 0 <= y < self.y
+        return 0 <= x < self.width and 0 <= y < self.height
 
     def random_point(self):
-        return randint(0, self.x -1), randint(0, self.y - 1)
+        return random.randint(0, self.width - 1), random.randint(0, self.height - 1)
 
-    def neighbors(self, x, y):
-        return [(x + step[0], y + step[1]) for step in steps_9_way()]
-     
+    def neighbors_inclusive(self, x, y):
+        return [(x + step[0], y + step[1]) for step in steps_9_way()]    
+
+    def neighbors_exclusive(self, x, y):
+        return [(x + step[0], y + step[1]) for step in steps_8_way()]    
+
+    def neighbors_lateral(self, x, y):
+        return [(x + step[0], y + step[1]) for step in steps_4_lateral()]    
+
+    def neighbors_diagonal(self, x, y):
+        return [(x + step[0], y + step[1]) for step in steps_4_diagonal()]    
+
+    def generate(self):
+        raise NotImplementedError('Use child map class instead')
+
+    def maxmin(self):
+        height = set()
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.world[y][x] not in height:
+                    height.add(self.world[y][x])
+
+        heights = sorted(height, reverse=True)
+        self.max = heights[0]
+        self.min = heights[-1]
+
     def smooth(self):
-        world = [[0 for _ in range(self.x)] for _ in range(self.y)]
-        for y in range(self.y):
-            for x in range(self.x):
+        world = [[0 for _ in range(self.width)] for _ in range(self.height)]
+        for y in range(self.height):
+            for x in range(self.width):
                 num = 0
                 value = 0
-                for xx, yy in self.neighbors(x, y):
+                for xx, yy in self.neighbors_inclusive(x, y):
                     try:
                         value += self.world[yy][xx]
                         num += 1
                     except IndexError:
                         pass
-
-                world[y][x] = value/num
+                world[y][x] = value / num
         self.world = world
 
-    def build(self, seed=None): 
+    def normalize(self):
+        if not hasattr(self, 'max'):
+            self.maxmin()
+
+        world = [[0 for _ in range(self.width)] for _ in range(self.height)]
+        for y in range(self.height):
+            for x in range(self.width):
+                world[y][x] = int((self.world[y][x] / self.max) * 250)
+        self.world = world
+        self.maxmin()
+
+    def integer_to_hex(self, value):
+        value = hex(value).split('x')[1]
+        if len(value) < 2:
+            value = "0" + value
+        return '#' + value * 3
+
+    def split_range(self, number=25):
+        return sorted([(self.max * i) / number for i in range(number)], reverse=True)
+
+    def evaluate(self):
+        if not hasattr(self, 'max'):
+            self.maxmin()
+
+        for y in range(self.height):
+            for x in range(self.width):
+                if 215 <= self.world[y][x]:
+                    value = ('^', self.integer_to_hex(self.world[y][x]))
+                    self.world_colored[y][x] = value
+                    self.world_normal[y][x] = value
+
+                elif 130 <= self.world[y][x] < 215:
+                    value = ('^', self.integer_to_hex(self.world[y][x]))
+                    self.world_colored[y][x] = value
+                    self.world_normal[y][x] = value
+
+                elif 60 <= self.world[y][x] < 130:
+                    self.world_colored[y][x] = ('"', '#40FF40')
+                    self.world_normal[y][x] = ('"', self.integer_to_hex(self.world[y][x]))
+
+                elif 10 <= self.world[y][x] < 60:
+                    self.world_colored[y][x] = ('.', '#EEDD33')
+                    self.world_normal[y][x] = ('.', self.integer_to_hex(self.world[y][x]))
+
+                else:
+                    self.world_colored[y][x] = ('~', '#4040FF')
+                    self.world_normal[y][x] = ('~', self.integer_to_hex(self.world[y][x]))
+
+    def output_terminal(self, colored=False):
+        world = self.world_colored if colored else self.world_normal
+        for y in range(self.height):
+            for x in range(self.width):
+                yield (x, y, *world[y][x])
+
+    def output_image(self, colored=False):
+        world = self.world_colored if colored else self.world_normal
+        img = Image.new('RGB', (self.width * 8, self.height * 8))
+        ids = ImageDraw.Draw(img)
+        for y in range(self.height):
+            for x in range(self.width):
+                ids.rectangle(
+                    [x * 8, y * 8, x * 8 + 8, y * 8 + 8], world[y][x][1])
+        img.save(self.__class__.__name__ + '.png')        
+
+class Drunkards(Map):
+    def __init__(self, width, height, limit, seed=None):
+        super().__init__(width, height, seed)
+        self.limit = int(height * width * limit)
+
+    def generate(self): 
         """ Returns map filled with drunkards height algo """
         self.spaces = set()
-        steps = steps_4_way()
         ry, rx = self.random_point()
 
         while len(self.spaces) <= self.limit:
-            step = choice(list(steps))
+            step = random.choice(list(steps_4_lateral()))
             # if at somepoint we are at the edge of the map and choose
             # a point outside of the map bounds we choose a random point
             # from the map to start the process again
@@ -85,105 +220,74 @@ class Map:
             self.world[ry][rx] += 1
             self.spaces.add((rx, ry))
 
-        # Can smooth more than once but each smooth makes the terrain
-        # flatter and flatter
-        self.smooth()
+class DrunkardsPeaks(Map):
+    def __init__(self, width, height, limit, peaks, seed=None):
+        """Drunkards algorithm with peaks"""
+        super().__init__(width, height, seed)
+        self.limit = int(width * height * limit)
+        self.peaks = [(
+            random.randint(width // 2 - width // 4, width // 2 + width // 4), 
+            random.randint(height // 2 - height // 3, height // 2 + height // 3)) 
+                for _ in range(peaks)]
 
-        # This double for loop puts all heights into the height set
-        # for evaluation and analysis purposes
-        # We do this after for loop so it doesn't hold every single 
-        # number possible starting from 0
-        height = set()
-        for y in range(self.y):
-            for x in range(self.x):
-                if self.world[y][x] not in height:
-                    height.add(self.world[y][x])
+    def random_point(self):
+        # return randint(0, self.x -1), randint(0, self.y - 1)
+        return self.peaks[random.randint(0, len(self.peaks) - 1)]
 
-        self.z = sorted(height, reverse=True)[0]
-        self.evaluate()
+    def generate(self):
+        self.spaces = set()
+        rx, ry = self.random_point()
+        while len(self.spaces) <= self.limit:
+            step = random.choice(list(steps_4_lateral()))
+            if self.check_bounds(rx + step[0], ry + step[1]):
+                rx, ry = rx + step[0], ry + step[1]
+            else:
+                rx, ry = self.random_point()
 
-    def split_range(self, number=10):
-        if not hasattr(self, 'z'):
-            raise AttributeError("Map not built yet -- call build()")
+            self.world[ry][rx] += 1
+            self.spaces.add((rx, ry))
 
-        return sorted([(self.z * i) // number for i in range(10)], reverse=True)
-
-    def integer_to_hex(self, value):
-        value = hex(value).split('x')[1]
-        if len(value) < 2:
-            value = "0" + value
-        return '#' + value * 3
-
-    def normalize(self, value):
-        return int((value / self.z) * 250)
-
-    def evaluate(self):
-        if not hasattr(self, 'z'):
-            raise AttributeError("Map not built yet -- call build()")
-
-        self.world_normal = [[0 for _ in range(self.x)] for _ in range(self.y)]
-        self.world_colored = [[0 for _ in range(self.x)] for _ in range(self.y)]
-
-        ranges = self.split_range()
-        a, b, c, d = 2, 5, 7, 8
-        
-        for y in range(self.y):
-            for x in range(self.x):
-                if self.world[y][x] >= ranges[a]:
-                    self.world_colored[y][x] = ('A', '#FFFFFF')
-                    self.world_normal[y][x] = ('A', self.integer_to_hex(self.normalize(self.world[y][x])))
-
-                elif ranges[b] <= self.world[y][x] < ranges[a]:
-                    self.world_colored[y][x] = ('n', '#808080')
-                    self.world_normal[y][x] = ('A', self.integer_to_hex(self.normalize(self.world[y][x])))
-
-                elif ranges[c] <= self.world[y][x] < ranges[b]:
-                    self.world_colored[y][x] = ('*', '#40FF40')
-                    self.world_normal[y][x] = ('*', self.integer_to_hex(self.normalize(self.world[y][x])))
-
-                elif ranges[d] <= self.world[y][x] < ranges[c]:
-                    self.world_colored[y][x] = ('.', '#80FF80')
-                    self.world_normal[y][x] = ('.', self.integer_to_hex(self.normalize(self.world[y][x])))
-
-                else:
-                    self.world_colored[y][x] = ('~', '#4040FF')
-                    self.world_normal[y][x] = ('~', self.integer_to_hex(self.normalize(self.world[y][x])))
-
-    def output_terminal(self, colored=False):
-        world = self.world_colored if colored else self.world_normal
-        for y in range(self.y):
-            for x in range(self.x):
-                yield (x, y, *world[y][x])
-
-    def output_image(self, colored=False):
-        world = self.world_colored if colored else self.world_normal
-        img = Image.new('RGB', (self.x * 8, self.y * 8))
-        ids = ImageDraw.Draw(img)
-        for y in range(self.y):
-            for x in range(self.x):
-                ids.rectangle(
-                    [x * 8, y * 8, x * 8 + 8, y * 8 + 8], world[y][x][1])
-        img.save('drunkards.png')
-
-if __name__ == "__main__":
+def test_drunkards():
     width, height = 160, 50
     setup(width, height)
-    m = Map(width, height, .3)
-    m.build()
-    output_flag = 1
-    while True:
-        # console.clear()
-        terminal.clear()
-        for x, y, ch, col in list(m.output_terminal()):
-            terminal.puts(x, y, '[c={}]{}[/c]'.format(col, ch))
-        terminal.refresh()
 
-        key = terminal.read()
-        if key_handle_exit(key):
-            break
-        elif key == terminal.TK_F:
-            output_flag *= -1
-            if output_flag > 0:
-                method = m.output()
-            else:
-                method = m.output_color()
+    m = Drunkards(width, height, .45)
+    m.generate()
+    # m.smooth()
+    # m.smooth()
+    m.normalize()
+    m.evaluate()
+
+    term_loop(m)
+
+def test_drunkards_peaks():
+    width, height = 160, 50
+    setup(width, height)
+
+    m = DrunkardsPeaks(width, height, .3, 7)
+    m.generate()
+    m.normalize()
+    m.evaluate()
+
+    term_loop(m)
+
+def test_combination():
+    width, height = 200, 60
+    setup(width, height)
+    
+    m = Drunkards(width, height, .45)
+    n = DrunkardsPeaks(width, height, .45, 13)  
+
+    m.generate()
+    n.generate()
+    
+    m *= n
+
+    m.normalize()
+    m.evaluate()
+    term_loop(m)
+
+if __name__ == "__main__":
+    # test_drunkards()
+    # test_drunkards_peaks()
+    test_combination()
