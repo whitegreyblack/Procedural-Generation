24855392d52e98f4b23f8d988630c4196b5c378b
Sam Whang <sangwoowhang@gmail.com>
2017-12-21 14:45:42 -0600

    Added more assertions in the combinations testing

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,6 @@
 import drunkards
 from random import random, randint
+from functools import reduce
 
 def test_constructor_single_callable_with_callable_args():
     width = 10
@@ -102,7 +103,9 @@ def test_combinations_horizontal_inclusive():
     x, y = (5, 5)
     comb_type = drunkards.Combinations.HORIZONTAL
     steps = drunkards.Combinations.combinations(comb_type, inclusive=True)
+    assert 0 == sum(reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), steps))
     assert len(steps) == 3
+    assert (0, 0) in steps
     for xx, yy in steps:
         assert x - 1 <= x + xx <= x + 1
         assert y + yy == y
@@ -111,7 +114,9 @@ def test_combinations_horizontal_exclusive():
     x, y = (5, 5)
     comb_type = drunkards.Combinations.HORIZONTAL
     steps = drunkards.Combinations.combinations(comb_type, inclusive=False)
+    assert 0 == sum(reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), steps))
     assert len(steps) == 2
+    assert (0, 0) not in steps
     for xx, yy in steps:
         assert x - 1 <= x + xx <= x + 1
         assert y + yy == y
@@ -120,7 +125,9 @@ def test_combinations_vertical_inclusive():
     x, y = (5, 5)
     comb_type = drunkards.Combinations.VERTICAL
     steps = drunkards.Combinations.combinations(comb_type, inclusive=True)
+    assert 0 == sum(reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), steps))
     assert len(steps) == 3
+    assert (0, 0) in steps
     for xx, yy in steps:
         assert x + xx == x
         assert x -1 <= x + xx <= x + 1
@@ -129,6 +136,7 @@ def test_combinations_vertical_exclusive():
     x, y = (5, 5)
     comb_type = drunkards.Combinations.VERTICAL
     steps = drunkards.Combinations.combinations(comb_type, inclusive=False)
+    assert 0 == sum(reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), steps))
     assert len(steps) == 2
     assert (0, 1) in steps
     for xx, yy in steps:
@@ -139,7 +147,9 @@ def test_combinations_diagonal_exclusive():
     x, y = (5, 5)
     comb_type = drunkards.Combinations.DIAGONAL
     steps = drunkards.Combinations.combinations(comb_type, inclusive=False)
+    assert 0 == sum(reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), steps))
     assert len(steps) == 4
+    assert (0, 0) not in steps
     for xx, yy in steps:
         assert x -1 <= x + xx <= x + 1
         assert y -1 <= y + yy <= y + 1
@@ -148,6 +158,18 @@ def test_combinations_diagonal_inclusive():
     x, y = (5, 5)
     comb_type = drunkards.Combinations.DIAGONAL
     steps = drunkards.Combinations.combinations(comb_type, inclusive=True)
+    assert 0 == sum(reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), steps))
+    assert len(steps) == 5
+    assert (0, 0) in steps
+    for xx, yy in steps:
+        assert x -1 <= x + xx <= x + 1
+        assert y -1 <= y + yy <= y + 1
+
+def test_combinations_lateral_exclusive():
+    x, y = (5, 5)
+    comb_type = drunkards.Combinations.DIAGONAL
+    steps = drunkards.Combinations.combinations(comb_type, inclusive=True)
+    assert 0 == sum(reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), steps))
     assert len(steps) == 5
     assert (0, 0) in steps
     for xx, yy in steps:
