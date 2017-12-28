94d582589af8ec31976a6f00a17714cc062fbd59
whitegreyblack <sangwoowhang@gmail.com>
2017-12-27 23:38:40 -0800

    Added tests for color init function

---
+++ test_color.py
@@ -0,0 +1,29 @@
+from drunkards import Color
+
+def test_color_no_args():
+    c = Color()
+    assert c.color == "#000000"
+
+def test_color_single_arg():
+    c = Color(r=255)
+    assert c.color == "#ff0000"
+
+    c = Color(g=255)
+    assert c.color == "#00ff00"
+
+    c = Color(b=255)
+    assert c.color == "#0000ff"
+
+def test_color_double_args():
+    c = Color(r=255, g=255)
+    assert c.color == "#ffff00"
+
+    c = Color(r=255, b=255)
+    assert c.color == "#ff00ff"
+
+    c = Color(g=255, b=255)
+    assert c.color == "#00ffff"
+
+def test_color_triple_args():
+    c = Color(r=255, g=255, b=255)
+    assert c.color == "#ffffff"
